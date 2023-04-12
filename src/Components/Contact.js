43b84bb1353fbb76ace4657c8bf280a6dcdd6ee6
akshay pratap singh <akshaypratapsingh4545@gmail.com>
2023-04-12 10:17:23 +0530

    ol

---
+++ src/Components/Contact.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Nav from './Nav'
+
+const Contact = () => {
+  return (
+    <>
+      <Nav/>
+  
+      <h1>Contact US</h1> 
+    </>
+  )
+}
+
+export default Contact
